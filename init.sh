b40b3506c1fceae1d51195c35454f4fbd0226bb7
Alex Blease <alexblease@gmail.com>
2025-11-04 16:14:02 +0000

    Add common git aliases

--- init.sh
+++ init.sh
@@ -157,7 +157,11 @@ install_or_update_git() {
 configure_git() {
   log "Installing/upgrading Git via Homebrew..."
   git config --global user.name "Alex Blease"
-  git config --global user.email "alexblease@gmail.com" 
+  git config --global user.email "alexblease@gmail.com"
+  git config --global alias.st status
+  git config --global alias.co checkout
+  git config --global alias.ds "diff --staged"
+  git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 }
 
 main() {
